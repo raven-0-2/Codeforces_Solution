dcdcede28e9f9a85f678ebda46848cb16259aa66
Raven <redowan.i.sarker@gamil.com>
2024-12-27 14:57:11 +0600

    27-12-24

--- 900/tempCodeRunnerFile.cpp
+++ 900/tempCodeRunnerFile.cpp
@@ -1,1 +1,63 @@
-cout<<prm[i]<<nl;
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define ll long long
+#define YES "YES\n"
+#define NO "NO\n"
+#define Yes "Yes\n"
+#define No "No\n"
+#define ck cout << '*' << nl;
+#define While \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+
+void raven()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+
+    raven();
+
+   While
+   {
+    ll x,i,sum=0,ans=0;
+    cin>>x;
+    vector <ll> v;
+    for(i=0;i<x;i++)
+    {
+        ll temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(),v.end(),greater<ll>());
+    for(i=0;i<x;i++)
+    {
+        if(v[i] == v[i+1] && i !=x-1)
+        {
+            sum++;
+        } 
+        else if(v[i] != v[i+1] || i == x)
+        {
+            sum++;
+            if(sum%2 !=0)
+            {
+                cout<<YES;
+            }
+            else
+            {
+                cout<<NO;
+            }
+            break;
+        }
+    }
+   }
+    
+}
